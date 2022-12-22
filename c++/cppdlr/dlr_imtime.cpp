3378fbcad34a5ca4528fd357d4ae93a92f45b9ef
jasonkaye <jkaye@flatironinstitute.org>
2022-12-22 16:00:41 -0500

    Added imtime_ops class---forgot to include in previous commit. Still untested.

---
+++ c++/cppdlr/dlr_imtime.cpp
@@ -0,0 +1,106 @@
+#include "dlr_imtime.hpp"
+#include "cppdlr/dlr_kernels.hpp"
+#include "dlr_build.hpp"
+#include "utils.hpp"
+
+using namespace nda;
+
+static constexpr auto _  = range::all;
+static constexpr auto __ = nda::ellipsis();
+
+namespace cppdlr {
+
+  imtime_ops::imtime_ops(double lambda, nda::vector_const_view<double> dlr_rf) : dlr_rf(dlr_rf) {
+
+    r = dlr_rf.size();
+
+    dlr_it    = nda::vector<double>(r);
+    cf2it     = nda::matrix<double>(r, r);
+    it2cf.lu  = nda::matrix<double>(r, r);
+    it2cf.piv = nda::vector<int>(r);
+
+    // Get discretization of analytic continuation kernel on fine grid in
+    // imaginary time, at DLR frequencies
+
+    auto fine = fineparams(lambda);
+    auto t    = get_tfine(fine);
+    auto kmat = get_kfine(t, dlr_rf);
+
+    // Pivoted Gram-Schmidt to obtain DLR imaginary time nodes
+
+    auto [q, norms, piv] = pivrgs(kmat, 1e-100);
+    for (int i = 0; i < r; ++i) { dlr_it(i) = t(piv(i)); }
+
+    // Obtain coefficients to imaginary time values transformation matrix
+
+    for (int i = 0; i < r; ++i) {
+      for (int j = 0; j < r; ++j) { cf2it(i, j) = kmat(piv(i), j); }
+    }
+
+    // Prepare imaginary time values to coefficients transformation by computing
+    // LU factors of coefficient to imaginary time matrix
+
+    it2cf.lu = cf2it;
+
+    // [Q] How to get rid of compiler warnings about unused info variable?
+
+    int info = lapack::getrf(it2cf.lu, it2cf.piv);
+  }
+
+  nda::array<double, 3> imtime_ops::vals2coefs(nda::array_const_view<double, 3> g) {
+
+    // [Q] This is very ugly. Can this be improved? Need to jump through these
+    // hoops because (1) getrs must take in rank 2 array, and (2) getrs is using
+    // Fortran-ordering instead of C-ordering. Want to be able to just call
+    // lapack::getrs(it2cf.lu, g(__,_), it2cf.piv) and have it do the right
+    // thing, i.e., reshape as an norb^2 x r array, and do norb^2 rxr solves.
+
+    int norb = g.shape(0);
+    auto gc  = nda::array<double, 3>(norb, norb, r);
+
+    auto tmp = nda::array<double, 2>(r, 1);
+    int info = 0;
+    for (int i = 0; i < norb; ++i) {
+      for (int j = 0; j < norb; ++j) {
+        tmp(_, 1)   = g(i, j, _);
+        info        = lapack::getrs(it2cf.lu, tmp, it2cf.piv);
+        gc(i, j, _) = tmp(_, 1);
+      }
+    }
+    return gc;
+  }
+
+  nda::array<double, 3> imtime_ops::coefs2vals(nda::array_const_view<double, 3> gc) {
+    int norb = gc.shape(0);
+    auto g   = nda::array<double, 3>(norb, norb, gc.shape(2));
+
+    // [Q] Can I do this in LAPACK style with one line?
+    for (int i = 0; i < norb; ++i) {
+      for (int j = 0; j < norb; ++j) { g(i, j, _) = cf2it * nda::vector<double>(gc(i, j, _)); }
+    }
+    return g;
+  }
+
+  nda::matrix<double> imtime_ops::coefs2eval(nda::array_const_view<double, 3> gc, double t) {
+
+    int norb = gc.shape(0);
+
+    auto g = nda::matrix<double>(norb, norb);
+
+    double kval = 0;
+    if (t >= 0) {
+      for (int l = 0; l < r; ++l) {
+        kval = kfun_abs(t, dlr_rf(l));
+        g += kval * gc(__, l);
+      }
+    } else {
+      for (int l = 0; l < r; ++l) {
+        kval = kfun_abs(-t, -dlr_rf(l));
+        g += kval * gc(__, l);
+      }
+    }
+
+    return g;
+  }
+
+} // namespace cppdlr
---
+++ c++/cppdlr/dlr_imtime.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <nda/nda.hpp>
+
+namespace cppdlr {
+
+  /**
+   * Class responsible for all DLR imaginary time operations, including building
+   * imaginary time grid and transformations
+   */
+
+  class imtime_ops {
+
+    public:
+    imtime_ops(double lambda, nda::vector_const_view<double> dlr_rf);
+
+    /** Transform values of Green's function on DLR imaginary time grid to its DLR coefficients */
+    nda::array<double, 3> vals2coefs(nda::array_const_view<double, 3> g);
+
+    /** Transform DLR coefficients of Green's function to its values on DLR imaginary time grid */
+    nda::array<double, 3> coefs2vals(nda::array_const_view<double, 3> gc);
+
+    /** Least squares fit of Green's function given by imaginary time samples, returning its DLR coefficients */
+    nda::array<double, 3> data2coefs(nda::vector_const_view<double> t, nda::array_const_view<double, 3> g);
+
+    /** Evaluate DLR expansion given by its DLR coefficients at an imaginary time point */
+    nda::matrix<double> coefs2eval(nda::array_const_view<double, 3> gc, double t);
+
+    /** Evaluate DLR expansion given by its values on DLR imaginary time grid at an imaginary time points */
+    nda::matrix<double> vals2eval(nda::array_const_view<double, 3> gc);
+
+    private:
+    int r;                      /// DLR rank
+    nda::vector<double> dlr_rf; /// DLR frequencies
+    nda::vector<double> dlr_it; /// DLR imaginary time nodes
+    nda::matrix<double> cf2it;  /// Transformation matrix from DLR coefficients to values at DLR imaginary time nodes
+
+    struct {
+      nda::matrix<double> lu;
+      nda::vector<int> piv;
+    } it2cf; /// Transformation (represented by LU factors and pivots) from values at DLR imaginary time nodes to DLR coefficients
+  };
+
+} // namespace cppdlr
