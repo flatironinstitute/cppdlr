809fbbbd507456094c31ee3924379e02a033e772
jasonkaye <jkaye@flatironinstitute.org>
2023-03-08 12:00:25 -0500

    Templating for double or complex-valued Green's functions in imtime_ops class + cleaning and documentation

--- c++/cppdlr/dlr_imtime.cpp
+++ c++/cppdlr/dlr_imtime.cpp
@@ -19,92 +19,33 @@ namespace cppdlr {
 
     // Get discretization of analytic continuation kernel on fine grid in
     // imaginary time, at DLR frequencies
-
     auto fine = fineparams(lambda);
     auto t    = get_tfine(fine);
     auto kmat = get_kfine(t, dlr_rf);
 
     // Pivoted Gram-Schmidt to obtain DLR imaginary time nodes
-
     auto [q, norms, piv] = pivrgs(kmat, 1e-100);
     for (int i = 0; i < r; ++i) { dlr_it(i) = t(piv(i)); }
 
     // Obtain coefficients to imaginary time values transformation matrix
-
     for (int i = 0; i < r; ++i) {
       for (int j = 0; j < r; ++j) { cf2it(i, j) = kmat(piv(i), j); }
     }
 
     // Prepare imaginary time values to coefficients transformation by computing
     // LU factors of coefficient to imaginary time matrix
-
     it2cf.lu = cf2it;
-
     lapack::getrf(it2cf.lu, it2cf.piv);
   }
 
-  nda::matrix<double> imtime_ops::vals2coefs_mat(nda::matrix_const_view<double> g) {
-
-    // [Q] Transposes handled correctly?
-    auto gc = nda::matrix<double>(transpose(g));
-    lapack::getrs(it2cf.lu, gc, it2cf.piv);
-
-    return transpose(gc);
-  }
-
-  nda::matrix<double> imtime_ops::coefs2vals_mat(nda::matrix_const_view<double> gc) {
-
-    return cf2it * gc;
-
-  }
-
-
-  nda::vector<double> imtime_ops::coefs2eval_mat(nda::matrix_const_view<double> gc, double t) {
-
-    // TODO: this can be further optimized; for example, can reduce # of exponential evaluations.
-    auto kvec = nda::vector<double>(r);
-    if (t >= 0) {
-      for (int l = 0; l < r; ++l) {
-        kvec(l) = kfun_abs(t, dlr_rf(l));
-      }
-    } else {
-      for (int l = 0; l < r; ++l) {
-        kvec(l) = kfun_abs(-t, -dlr_rf(l));
-      }
-    }
-
-    return transpose(gc)*kvec;
-  }
-
-  double imtime_ops::coefs2eval_vec(nda::vector_const_view<double> gc, double t) {
-
-    // TODO: this can be further optimized; for example, can reduce # of exponential evaluations.
-    double g = 0;
-    if (t >= 0) {
-      for (int l = 0; l < r; ++l) {
-        g += kfun_abs(t, dlr_rf(l))*gc(l);
-      }
-    } else {
-      for (int l = 0; l < r; ++l) {
-        g += kfun_abs(-t, -dlr_rf(l))*gc(l);
-      }
-    }
-
-    return g;
-  }
-
-
   nda::vector<double> imtime_ops::get_kevalvec(double t) {
 
+    // TODO: can be further optimized to reduce # exponential evals.
     auto kvec = nda::vector<double>(r);
     if (t >= 0) {
-      for (int l = 0; l < r; ++l) {
-        kvec(l) = kfun_abs(t, dlr_rf(l));
-      }
+      for (int l = 0; l < r; ++l) { kvec(l) = kfun_abs(t, dlr_rf(l)); }
     } else {
-      for (int l = 0; l < r; ++l) {
-        kvec(l) = kfun_abs(-t, -dlr_rf(l));
-      }
+      for (int l = 0; l < r; ++l) { kvec(l) = kfun_abs(-t, -dlr_rf(l)); }
     }
 
     return kvec;
--- c++/cppdlr/dlr_imtime.hpp
+++ c++/cppdlr/dlr_imtime.hpp
@@ -1,130 +1,169 @@
 #pragma once
 #include <nda/nda.hpp>
+#include "cppdlr/dlr_kernels.hpp"
 
 namespace cppdlr {
 
   /**
-   * Class responsible for all DLR imaginary time operations, including building
-   * imaginary time grid and transformations
-   */
+  * @class imtime_ops
+  * @brief Class responsible for all DLR imaginary time operations, including
+  * building imaginary time grid and transformations.
+  *
+  * \note First dimension of all Green's function and coefficient arrays must be
+  * DLR rank r.
+  */
 
   class imtime_ops {
 
     public:
+    /** 
+    * @brief Constructor for imtime_ops
+    * 
+    * @param[in] lambda DLR cutoff parameter
+    * @param[in] dlr_rf DLR frequencies
+    */
     imtime_ops(double lambda, nda::vector_const_view<double> dlr_rf);
 
-    /** Transform values of Green's function on DLR imaginary time grid to its DLR coefficients */
-
-    // This method takes g of any type fulfilling the constraints of the NDA
-    // MemoryArray concept. In particular, it can be an NDA vector, matrix,
-    // array, or view of any of these. The notation T::regular_type indicates
-    // that if a view is passed in, a vector/matrix/array (and not a view of
-    // these) will still be returned. The only constraint is that the final
-    // index of g be of dimension r. So, for a matrix-valued Green's function
-    // G_ij(tau) with 1<=i,j<=n, we pass in an nxnxr array.
-
-    template <nda::MemoryArray T> 
+    /** 
+    * @brief Transform values of Green's function G on DLR imaginary time grid to
+    * DLR coefficients 
+    *
+    * @param[in] g Values of G on DLR imaginary time grid
+    *
+    * @return DLR coefficients of G
+    */
+    template <nda::MemoryArray T, typename S = nda::get_value_t<T>>
+      requires(nda::is_scalar_v<S>)
     typename T::regular_type vals2coefs(T const &g) {
+
+      // MemoryArray type can be nda vector, matrix, array, or view of any of
+      // these; taking a regular_type converts, for example, a matrix view to a
+      // matrix.
 
       if (r != g.shape(0)) throw std::runtime_error("First dim of g != DLR rank r.");
 
-      // First, we reshape g to a matrix with second dimension r. Then we call a
-      // vals->coefs function which operates on matrices. Then we return the
-      // result to its original shape. 
+      // Reshape g to matrix w/ second dimension r
+      auto g_rs = nda::reshaped_view(g, std::array<long, 2>{r, g.size() / r});
+      auto gct  = nda::matrix<S>(transpose(g_rs));
 
-      auto g_reshaped = nda::reshaped_view(g,std::array<long,2> {r,g.size()/r});
-      auto gc = vals2coefs_mat(g_reshaped);
-      // [Q] Why reshaped_view instead of reshape? But this works, and reshape doesn't compile.
-      return nda::reshaped_view(gc,g.shape());
+      // Solve linear system (multiple right hand sides) to convert vals ->
+      // coeffs (we transpose because LAPACK requires index into RHS # to be
+      // slowest)
+      nda::lapack::getrs(it2cf.lu, gct, it2cf.piv);
 
+      // Reshape to original dimensions and return
+      auto gc = nda::matrix<S>(transpose(gct));
+      return nda::reshaped_view(gc, g.shape());
     }
 
-    private:
-    nda::matrix<double> vals2coefs_mat(nda::matrix_const_view<double> g);
-
-    public:
-
-    /** Transform DLR coefficients of Green's function to its values on DLR imaginary time grid */
-
-    template <nda::MemoryArray T> 
+    /** 
+    * @brief Transform DLR coefficients of Green's function G to values on DLR
+    * imaginary time grid
+    *
+    * @param[in] gc DLR coefficients of G
+    *
+    * @return Values of G on DLR imaginary time grid
+    * */
+    template <nda::MemoryArray T, typename S = nda::get_value_t<T>>
+      requires(nda::is_scalar_v<S>)
     typename T::regular_type coefs2vals(T const &gc) {
 
       if (r != gc.shape(0)) throw std::runtime_error("First dim of g != DLR rank r.");
 
-      // First, we reshape gc to a matrix with second dimension r. Then we call a
-      // coefs->vals function which operates on matrices. Then we return the
-      // result to its original shape. 
+      // Reshape gc to a matrix w/ second dimension r
+      auto gc_rs = nda::reshaped_view(gc, std::array<long, 2>{r, gc.size() / r});
 
-      auto gc_reshaped = nda::reshaped_view(gc,std::array<long,2> {r, gc.size()/r});
-      auto g = coefs2vals_mat(gc_reshaped);
+      // Apply coeffs -> vals matrix
+      auto g = cf2it * nda::matrix_const_view<S>(gc_rs);
+
       // [Q] Why reshaped_view instead of reshape? But this works, and reshape doesn't compile.
-      return nda::reshaped_view(g,gc.shape());
-
+      // Reshape to original dimensions and return
+      return nda::reshaped_view(g, gc.shape());
     }
 
-    private:
-    nda::matrix<double> coefs2vals_mat(nda::matrix_const_view<double> gc);
-
-    public:
-
-    /** Least squares fit of Green's function given by imaginary time samples, returning its DLR coefficients */
-    nda::array<double, 3> data2coefs(nda::vector_const_view<double> t, nda::array_const_view<double, 3> g);
-
-    /** Evaluate DLR expansion given by its DLR coefficients at an imaginary time point */
-    
-    template <nda::MemoryArray T>
+    /** 
+    * @brief Evaluate DLR expansion of G given by DLR coefficients at imaginary
+    * time point 
+    *
+    * @param[in] gc DLR coefficients of G
+    * @param[in] t  Evaluation point
+    *
+    * @return Value of G at @p t
+    */
+    template <nda::MemoryArray T, typename S = nda::get_value_t<T>>
+      requires(nda::is_scalar_v<S>)
     auto coefs2eval(T const &gc, double t) {
 
       if (r != gc.shape(0)) throw std::runtime_error("First dim of g != DLR rank r.");
 
-      // Scalar-valued Green's functions are handled differently than matrix-valued Green's functions 
-      if constexpr(T::rank==1) {
-       return coefs2eval_vec(gc,t);
+      // Scalar-valued Green's functions are handled differently than matrix-valued Green's functions
+
+      if constexpr (T::rank == 1) {
+
+        // TODO: can be further optimized to reduce # exponential evals.
+        // Evaluate DLR expansion
+        S g = 0;
+        if (t >= 0) {
+          for (int l = 0; l < r; ++l) { g += kfun_abs(t, dlr_rf(l)) * gc(l); }
+        } else {
+          for (int l = 0; l < r; ++l) { g += kfun_abs(-t, -dlr_rf(l)) * gc(l); }
+        }
+
+        return g;
       } else {
 
-      // First, we reshape gc to a matrix with second dimension r. Then we call a
-      // coefs->value function which operates on matrices and returns a vector. Then we return the
-      // result to its original shape (leaving out the final dimension, which is summed out). 
+        // Reshape gc to matrix w/ second dimension r
+        auto gc_rs = nda::reshaped_view(gc, std::array<long, 2>{r, gc.size() / r});
 
-      auto gc_reshaped = nda::reshaped_view(gc,std::array<long,2> {r,gc.size()/r});
+        // Get output shape
+        std::array<long, T::rank - 1> shape_out;
+        for (int i = 0; i < T::rank - 1; ++i) { shape_out[i] = gc.shape(i + 1); }
 
-      std::array<long,T::rank - 1> shape_out;
-      for (int i = 0; i < T::rank - 1; ++i) { shape_out[i] = gc.shape(i+1); }
+        // Get vector of evaluation of DLR expansion at a point
+        auto kvec = get_kevalvec(t);
 
-      return reshape(coefs2eval_mat(gc_reshaped,t),shape_out);
-
+        // Evaluate DLR expansion, reshape to original dimensions (with first
+        // dimension summed out), and return
+        return nda::reshape(transpose(nda::matrix_const_view<S>(gc_rs)) * kvec, shape_out);
       }
-
     }
 
-    private:
-    double coefs2eval_vec(nda::vector_const_view<double> gc, double t);
-    nda::vector<double> coefs2eval_mat(nda::matrix_const_view<double> gc, double t);
-
-    public:
-
-    /** Get vector of evaluation of DLR expansion at a point **/
+    /** 
+    * @brief Get vector of evaluation of DLR expansion at a point 
+    *
+    * @param[in] t  Evaluation point
+    *
+    * @return Vector of evaluation at @p t
+    **/
     nda::vector<double> get_kevalvec(double t);
 
-    /** Evaluate DLR expansion given by its values on DLR imaginary time grid at an imaginary time point */
-    nda::matrix<double> vals2eval(nda::array_const_view<double, 3> g);
+    /** 
+    * @brief Get DLR imaginary time nodes
+    *
+    * @return DLR imaginary time nodes
+    */
+    nda::vector_const_view<double> get_itnodes() const { return dlr_it; };
 
-    /** Access DLR imaginary time nodes*/
-    nda::vector_const_view<double> get_itnodes() const {return dlr_it; };
-
-    /** DLR rank **/
+    /** 
+    * @brief Get DLR rank
+    *
+    * @return DLR rank
+    */
     int rank() const { return r; }
 
     private:
-    int r;                      /// DLR rank
-    nda::vector<double> dlr_rf; /// DLR frequencies
-    nda::vector<double> dlr_it; /// DLR imaginary time nodes
-    nda::matrix<double> cf2it;  /// Transformation matrix from DLR coefficients to values at DLR imaginary time nodes
+    int r;                      ///< DLR rank
+    nda::vector<double> dlr_rf; ///< DLR frequencies
+    nda::vector<double> dlr_it; ///< DLR imaginary time nodes
+    nda::matrix<double> cf2it;  ///< Transformation matrix from DLR coefficients to values at DLR imaginary time nodes
 
+    /**
+    * Struct for transformation from DLR imaginary time values to coefficients
+    */
     struct {
-      nda::matrix<double> lu;
-      nda::vector<int> piv;
-    } it2cf; /// Transformation (represented by LU factors and pivots) from values at DLR imaginary time nodes to DLR coefficients
+      nda::matrix<double> lu;   ///< LU factors (LAPACK format) of imaginary time vals -> coefs matrix
+      nda::vector<int> piv;     ///< LU pivots (LAPACK format) of imaginary time vals -> coefs matrix
+    } it2cf; 
   };
 
 } // namespace cppdlr
