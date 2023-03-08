418c35d2766ca8b021338777fecfd84c54976ea9
jasonkaye <jkaye@flatironinstitute.org>
2023-03-08 16:49:17 -0500

    Templated pivrgs to take real or complex-valued matrices, and added test

--- c++/cppdlr/utils.hpp
+++ c++/cppdlr/utils.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <vector>
 #include <nda/nda.hpp>
+#include <nda/blas.hpp>
+
+using namespace nda;
 
 namespace cppdlr {
 
@@ -29,7 +32,103 @@ namespace cppdlr {
     const int n;            /// Chebyshev n
   };
 
-  std::tuple<nda::matrix<double>, nda::vector<double>, nda::vector<int>> pivrgs(nda::matrix<double> a, double eps);
+  /** 
+   * @brief Rank-revealing pivoted reorthogonalized Gram-Schmidt
+   *
+   * Determine the epsilon-rank of a matrix and return an orthogonal basis of
+   * its epsilon-row space.
+   *
+   * This is a translation of the Fortran subroutine "qrdgrm" by V.  Rokhlin.
+   *
+   * @param a   Matrix to be orthogonalized
+   * @param eps Rank cutoff tolerance
+   *
+   * @return Tuple of (1) matrix containing whose rows form orthogonal basis of
+   * row space of @p a to @p eps tolerance, (2) vector with entry n given by the
+   * squared l2 norm of the orthogonal complement of nth selected row with
+   * respect to subspace spanned by first n-1 selected rows, (3) vector of
+   * pivots
+   */
+  // Type T must be scalar-valued rank 2 array/array_view or matrix/matrix_view
+  template <nda::MemoryArrayOfRank<2> T, typename S = get_value_t<T>>
+    requires(nda::is_scalar_v<S>)
+  std::tuple<typename T::regular_type, nda::vector<double>, nda::vector<int>> pivrgs(T const &a, double eps) {
+
+    auto _ = nda::range::all;
+
+    // Copy input data
+    auto aa = make_regular(a);
+
+    // Get matrix dimensions
+    auto [m, n] = aa.shape();
+
+    // Compute norms of rows of input matrix, and rescale eps tolerance
+    auto norms     = nda::vector<double>(m);
+    double epsscal = 0; // Scaled eps threshold parameter
+    for (int j = 0; j < m; ++j) {
+      norms(j) = real(blas::dotc(aa(j, _), aa(j, _)));
+      // TODO: Need to choose between these; this choice is consistent w/ libdlr
+      //epsscal += norms(j);
+      epsscal = std::max(epsscal, norms(j));
+    }
+    epsscal *= eps * eps;
+
+    // Begin pivoted double Gram-Schmidt procedure
+    int jpiv = 0, jj = 0;
+    double nrm = 0;
+    auto piv   = nda::arange(0, m);
+    auto tmp   = nda::vector<S>(n);
+
+    for (int j = 0; j < m; ++j) {
+
+      // Find next pivot
+      jpiv = j;
+      nrm  = norms(j);
+      for (int k = j + 1; k < m; ++k) {
+        if (norms(k) > nrm) {
+          jpiv = k;
+          nrm  = norms(k);
+        }
+      }
+
+      // Swap current row with chosen pivot row
+      tmp        = aa(j, _);
+      aa(j, _)    = aa(jpiv, _);
+      aa(jpiv, _) = tmp;
+
+      nrm         = norms(j);
+      norms(j)    = norms(jpiv);
+      norms(jpiv) = nrm;
+
+      jj        = piv(j);
+      piv(j)    = piv(jpiv);
+      piv(jpiv) = jj;
+
+      // Orthogonalize current rows (now the chosen pivot row) against all
+      // previously chosen rows
+      for (int k = 0; k < j; ++k) { aa(j, _) = aa(j, _) - aa(k, _) * blas::dotc(aa(k, _), aa(j, _)); }
+
+      // Get norm of current row
+      nrm = real(blas::dotc(aa(j, _), aa(j, _)));
+      //nrm = nda::norm(aa(j, _));
+
+      // Terminate if sufficiently small, and return previously selected rows
+      // (not including current row)
+      if (nrm <= epsscal) { return {aa(range(0, j), _), norms(range(0, j)), piv(range(0, j))}; };
+
+      // Normalize current row
+      aa(j, _) = aa(j, _) * (1 / sqrt(nrm));
+
+      // Orthogonalize remaining rows against current row
+      for (int k = j + 1; k < m; ++k) {
+        if (norms(k) <= epsscal) { continue; } // Can skip rows with norm less than tolerance
+        aa(k, _)  = aa(k, _) - aa(j, _) * blas::dotc(aa(j, _), aa(k, _));
+        norms(k) = real(blas::dotc(aa(k, _), aa(k, _)));
+      }
+    }
+
+    return {aa, norms, piv};
+  }
 
   nda::vector<double> eqptsrel(int n);
 
--- test/c++/pivrgs.cpp
+++ test/c++/pivrgs.cpp
@@ -6,13 +6,15 @@
 using namespace cppdlr;
 using namespace nda;
 
-
-// Test pivoted reorthogonalized Gram-Schmidt
-
-TEST(pivrgs, pivrgs) {
+/**
+ * Test pivoted reorthogonalized Gram-Schmidt function pivrgs for real-valued
+ * matrix
+ */
+TEST(pivrgs, pivrgs_real) {
 
   auto _ = nda::range::all;
 
+  // Matrix size and rank cutoff tolerance
   int m      = 50;
   int n      = 40;
   double eps = 1e-6;
@@ -21,14 +23,12 @@ TEST(pivrgs, pivrgs) {
   // values 2^{-k}
 
   // Get random matrices
-
   auto a1 = nda::matrix<double>::rand(m, m);
   auto a2 = nda::matrix<double>::rand(n, n);
 
   // Orthonormalize to get random orthogonal matrices U, V. Note: this also
   // tests whether behavior of pivrgs is correct for numerically full rank
   // matrix.
-
   auto [u, norms1, piv1] = pivrgs(a1, 1e-100);
   auto [v, norms2, piv2] = pivrgs(a2, 1e-100);
 
@@ -36,36 +36,29 @@ TEST(pivrgs, pivrgs) {
   EXPECT_EQ(v.shape(), (std::array<long, 2>{n, n}));
 
   // Check u and v are orthogonal as well
-
   EXPECT_LE(frobenius_norm(eye<double>(m) - transpose(u) * u), 1e-14);
   EXPECT_LE(frobenius_norm(eye<double>(n) - transpose(v) * v), 1e-14);
 
   // Multiply smaller matrix by singular values S
-
   for (int i = 0; i < n; ++i) { v(i, _) *= pow(2.0, -i); }
 
   // Matrix A = U*S*V has given singular values
-
   auto a = u(_, range(0, n)) * v;
 
   // Gram-Schmidt to obtain orthonormal basis Q of row space of A
-
   auto [q, norms, piv] = pivrgs(a, eps);
   int r                = norms.size(); // Estimated rank
 
   // Verify rank is almost correct
-
   EXPECT_LE(r, ceil(log2(1.0 / eps)) + 3);
   EXPECT_GE(r, ceil(log2(1.0 / eps)) - 3);
 
   // Verify rows of Q are orthonormal to near double precision
-
   EXPECT_LE(frobenius_norm(eye<double>(r) - q * transpose(q)), 1e-14);
 
   // Verify row space of A contained in that of Q; make sure projection onto
   // row(Q) of random linear combination of rows of A yields identity to
   // within roughly target accuracy.
-
   auto x = nda::vector<double>::rand(m);
   x      = 2 * x - 1;
   x /= sqrt(blas::dot(x, x));
@@ -73,17 +66,15 @@ TEST(pivrgs, pivrgs) {
 
   auto tmp = make_regular(b - transpose(q) * (q * b));
 
-  EXPECT_LT(sqrt(blas::dot(tmp,tmp)), 10 * eps);
+  EXPECT_LT(sqrt(blas::dot(tmp, tmp)), 10 * eps);
 
   // More stringent test: make sure projection of A onto row space of Q is A
   // to within roughly target accuracy.
-
   EXPECT_LT(frobenius_norm(a - (a * transpose(q)) * q), 10 * eps);
 
   // Verify that calling pivoted GS on matrix of pivot rows yields an
   // identical Q matrix, and the correct order of pivots; this tests that the
   // function returns the correct pivots
-
   auto athin = nda::matrix<double>(r, n);
   for (int i = 0; i < r; ++i) { athin(i, _) = a(piv(i), _); }
 
@@ -94,3 +85,81 @@ TEST(pivrgs, pivrgs) {
   EXPECT_LE(frobenius_norm(q - qthin), 1e-14);
 }
 
+/**
+ * Test pivoted reorthogonalized Gram-Schmidt function pivrgs for complex-valued
+ * matrix
+ */
+TEST(pivrgs, pivrgs_cplx) {
+
+  auto _ = nda::range::all;
+
+  // Matrix size and rank cutoff tolerance
+  int m      = 50;
+  int n      = 40;
+  double eps = 1e-6;
+
+  // Generate random numerically low rank mxn matrix: rapidly decaying singular
+  // values 2^{-k}
+
+  // Get random matrices
+  auto a1 = nda::matrix<dcomplex>::rand(m, m);
+  auto a2 = nda::matrix<dcomplex>::rand(n, n);
+
+  // Orthonormalize to get random orthogonal matrices U, V. Note: this also
+  // tests whether behavior of pivrgs is correct for numerically full rank
+  // matrix.
+  auto [u, norms1, piv1] = pivrgs(a1, 1e-100);
+  auto [v, norms2, piv2] = pivrgs(a2, 1e-100);
+
+  EXPECT_EQ(u.shape(), (std::array<long, 2>{m, m}));
+  EXPECT_EQ(v.shape(), (std::array<long, 2>{n, n}));
+
+  // Check u and v are orthogonal as well
+  EXPECT_LE(frobenius_norm(eye<double>(m) - conj(transpose(u)) * u), 1e-14);
+  EXPECT_LE(frobenius_norm(eye<double>(n) - conj(transpose(v)) * v), 1e-14);
+
+  // Multiply smaller matrix by singular values S
+  for (int i = 0; i < n; ++i) { v(i, _) *= pow(2.0, -i); }
+
+  // Matrix A = U*S*V has given singular values
+  auto a = u(_, range(0, n)) * v;
+
+  // Gram-Schmidt to obtain orthonormal basis Q of row space of A
+  auto [q, norms, piv] = pivrgs(a, eps);
+  int r                = norms.size(); // Estimated rank
+
+  // Verify rank is almost correct
+  EXPECT_LE(r, ceil(log2(1.0 / eps)) + 3);
+  EXPECT_GE(r, ceil(log2(1.0 / eps)) - 3);
+
+  // Verify rows of Q are orthonormal to near double precision
+  EXPECT_LE(frobenius_norm(eye<double>(r) - q * conj(transpose(q))), 1e-14);
+
+  // Verify row space of A contained in that of Q; make sure projection onto
+  // row(Q) of random linear combination of rows of A yields identity to
+  // within roughly target accuracy.
+  auto x = nda::vector<dcomplex>::rand(m);
+  x      = 2 * x - 1;
+  x /= sqrt(real(blas::dotc(x, x)));
+  auto b = conj(transpose(a)) * x;
+
+  auto tmp = make_regular(b - conj(transpose(q)) * (q * b));
+
+  EXPECT_LT(sqrt(real(blas::dotc(tmp, tmp))), 10 * eps);
+
+  // More stringent test: make sure projection of A onto row space of Q is A
+  // to within roughly target accuracy.
+  EXPECT_LT(frobenius_norm(a - (a * conj(transpose(q))) * q), 10 * eps);
+
+  // Verify that calling pivoted GS on matrix of pivot rows yields an
+  // identical Q matrix, and the correct order of pivots; this tests that the
+  // function returns the correct pivots
+  auto athin = nda::matrix<dcomplex>(r, n);
+  for (int i = 0; i < r; ++i) { athin(i, _) = a(piv(i), _); }
+
+  auto [qthin, normthin, pivthin] = pivrgs(athin, eps);
+
+  EXPECT_EQ(pivthin.size(), r);
+  EXPECT_EQ(pivthin, arange(r));
+  EXPECT_LE(frobenius_norm(q - qthin), 1e-14);
+}
