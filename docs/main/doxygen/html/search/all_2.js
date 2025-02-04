6051e20c83836af3c2af163d5c27ad875f3ce5b3
jasonkaye <jkaye@flatironinstitute.org>
2025-02-04 14:30:10 +0000

    Deploying to github.io from @ flatironinstitute/cppdlr@e18a5694a07c52c50aaf2a9cffd91a4d944bf614 🚀

--- docs/main/doxygen/html/search/all_2.js
+++ docs/main/doxygen/html/search/all_2.js
@@ -4,8 +4,9 @@ var searchData=
   ['coefs2eval_1',['coefs2eval',['../classcppdlr_1_1imfreq__ops.html#a2ac467d6a8903940482bad858896d52c',1,'cppdlr::imfreq_ops::coefs2eval(double beta, T const &amp;gc, int n) const'],['../classcppdlr_1_1imfreq__ops.html#ae8bc361ee298e53554c7c664e3cbf8be',1,'cppdlr::imfreq_ops::coefs2eval(T const &amp;gc, int n) const'],['../classcppdlr_1_1imtime__ops.html#a12761a877407fc347dc695da4b2d7f3d',1,'cppdlr::imtime_ops::coefs2eval()']]],
   ['coefs2vals_2',['coefs2vals',['../classcppdlr_1_1imfreq__ops.html#a3179bf998af8b0a3c71fe7d333f0e44e',1,'cppdlr::imfreq_ops::coefs2vals(double beta, T const &amp;gc) const'],['../classcppdlr_1_1imfreq__ops.html#a4b37513f8cb287ea7033cc4d78f6157f',1,'cppdlr::imfreq_ops::coefs2vals(T const &amp;gc) const'],['../classcppdlr_1_1imtime__ops.html#a6f6d71c061aec134df9edf1255e19dfb',1,'cppdlr::imtime_ops::coefs2vals(T const &amp;gc) const']]],
   ['convmat_3',['convmat',['../classcppdlr_1_1imtime__ops.html#a6c0a93f188f4522b2389e9a7fcbf9783',1,'cppdlr::imtime_ops']]],
-  ['convolve_4',['convolve',['../classcppdlr_1_1imtime__ops.html#a4f018cf316547b2879d9a2dd76a4b80c',1,'cppdlr::imtime_ops::convolve(double beta, statistic_t statistic, T const &amp;fc, T const &amp;gc, bool time_order=false) const'],['../classcppdlr_1_1imtime__ops.html#a87b3e21ae66c97691a4580335795f77f',1,'cppdlr::imtime_ops::convolve(Tf const &amp;fconv, Tg const &amp;g) const']]],
-  ['convolve_5finit_5',['convolve_init',['../classcppdlr_1_1imtime__ops.html#a6257e0b3f43733c5ac06c8af11a47bb3',1,'cppdlr::imtime_ops']]],
-  ['cppdlr_6',['cppdlr',['../namespacecppdlr.html',1,'']]],
-  ['cppdlr_2ehpp_7',['cppdlr.hpp',['../cppdlr_8hpp.html',1,'']]]
+  ['convmat_5finplace_4',['convmat_inplace',['../classcppdlr_1_1imtime__ops.html#a9f48c8e4b3164985af73fba0fd28b002',1,'cppdlr::imtime_ops']]],
+  ['convolve_5',['convolve',['../classcppdlr_1_1imtime__ops.html#a4f018cf316547b2879d9a2dd76a4b80c',1,'cppdlr::imtime_ops::convolve(double beta, statistic_t statistic, T const &amp;fc, T const &amp;gc, bool time_order=false) const'],['../classcppdlr_1_1imtime__ops.html#a87b3e21ae66c97691a4580335795f77f',1,'cppdlr::imtime_ops::convolve(Tf const &amp;fconv, Tg const &amp;g) const']]],
+  ['convolve_5finit_6',['convolve_init',['../classcppdlr_1_1imtime__ops.html#a6257e0b3f43733c5ac06c8af11a47bb3',1,'cppdlr::imtime_ops']]],
+  ['cppdlr_7',['cppdlr',['../namespacecppdlr.html',1,'']]],
+  ['cppdlr_2ehpp_8',['cppdlr.hpp',['../cppdlr_8hpp.html',1,'']]]
 ];
