7a4f8c575eaf69af0f658de342fd75c8f1f8d77c
jasonkaye <jkaye@flatironinstitute.org>
2026-06-25 16:12:06 +0000

    Deploying to github.io from @ flatironinstitute/cppdlr@5f9adf91d7620ce145ead8e4e906151493038af3 🚀

--- docs/1.3.x/doxygen/html/search/all_5.js
+++ docs/1.3.x/doxygen/html/search/all_5.js
@@ -5,8 +5,9 @@ var searchData=
   ['fine_20real_20frequency_20grid_20panels_2',['fine real frequency grid panels',['../classcppdlr_1_1fineparams.html#autotoc_md0',1,'']]],
   ['fineparams_3',['fineparams',['../classcppdlr_1_1fineparams.html',1,'cppdlr::fineparams'],['../classcppdlr_1_1fineparams.html#a1e82a79933c91abe609a6e5c04e9bca9',1,'cppdlr::fineparams::fineparams()']]],
   ['fitvals2coefs_4',['fitvals2coefs',['../classcppdlr_1_1imtime__ops.html#a6a81a99b2858defa9513a395bd35e2bf',1,'cppdlr::imtime_ops']]],
-  ['free_5fgf_5',['free_gf',['../namespacecppdlr.html#a02424caf43ace0c3330b2fa03f706400',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, double mu=0, bool time_order=false)'],['../namespacecppdlr.html#a4b753b1a7de53aff215a230206908e6d',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, bool time_order)']]],
-  ['freq_20nodes_20different_20from_20r_20in_20symmetrized_20bosonic_20case_6',['DLR imaginary freq nodes (different from r in symmetrized bosonic case)',['../classcppdlr_1_1imfreq__ops.html#autotoc_md2',1,'']]],
-  ['frequency_20grid_20panels_7',['fine real frequency grid panels',['../classcppdlr_1_1fineparams.html#autotoc_md0',1,'']]],
-  ['from_20r_20in_20symmetrized_20bosonic_20case_8',['DLR imaginary freq nodes (different from r in symmetrized bosonic case)',['../classcppdlr_1_1imfreq__ops.html#autotoc_md2',1,'']]]
+  ['flip_5fto_5fmirror_5',['flip_to_mirror',['../namespacecppdlr.html#a721215e26c8f2e4de53d07f34c517823',1,'cppdlr']]],
+  ['free_5fgf_6',['free_gf',['../namespacecppdlr.html#a02424caf43ace0c3330b2fa03f706400',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, double mu=0, bool time_order=false)'],['../namespacecppdlr.html#a4b753b1a7de53aff215a230206908e6d',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, bool time_order)']]],
+  ['freq_20nodes_20different_20from_20r_20in_20symmetrized_20bosonic_20case_7',['DLR imaginary freq nodes (different from r in symmetrized bosonic case)',['../classcppdlr_1_1imfreq__ops.html#autotoc_md2',1,'']]],
+  ['frequency_20grid_20panels_8',['fine real frequency grid panels',['../classcppdlr_1_1fineparams.html#autotoc_md0',1,'']]],
+  ['from_20r_20in_20symmetrized_20bosonic_20case_9',['DLR imaginary freq nodes (different from r in symmetrized bosonic case)',['../classcppdlr_1_1imfreq__ops.html#autotoc_md2',1,'']]]
 ];
