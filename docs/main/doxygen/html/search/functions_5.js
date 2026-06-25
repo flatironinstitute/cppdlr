9659c73da6f2fbe7852b13e03a7c32fcf65989eb
Wentzell <Wentzell@users.noreply.github.com>
2026-06-25 15:24:56 +0000

    Deploying to github.io from @ flatironinstitute/cppdlr@5f9adf91d7620ce145ead8e4e906151493038af3 🚀

--- docs/main/doxygen/html/search/functions_5.js
+++ docs/main/doxygen/html/search/functions_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['fineparams_0',['fineparams',['../classcppdlr_1_1fineparams.html#a1e82a79933c91abe609a6e5c04e9bca9',1,'cppdlr::fineparams']]],
   ['fitvals2coefs_1',['fitvals2coefs',['../classcppdlr_1_1imtime__ops.html#a6a81a99b2858defa9513a395bd35e2bf',1,'cppdlr::imtime_ops']]],
-  ['free_5fgf_2',['free_gf',['../namespacecppdlr.html#a02424caf43ace0c3330b2fa03f706400',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, double mu=0, bool time_order=false)'],['../namespacecppdlr.html#a4b753b1a7de53aff215a230206908e6d',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, bool time_order)']]]
+  ['flip_5fto_5fmirror_2',['flip_to_mirror',['../namespacecppdlr.html#a721215e26c8f2e4de53d07f34c517823',1,'cppdlr']]],
+  ['free_5fgf_3',['free_gf',['../namespacecppdlr.html#a02424caf43ace0c3330b2fa03f706400',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, double mu=0, bool time_order=false)'],['../namespacecppdlr.html#a4b753b1a7de53aff215a230206908e6d',1,'cppdlr::free_gf(double beta, imtime_ops const &amp;itops, Ht const &amp;h, bool time_order)']]]
 ];
